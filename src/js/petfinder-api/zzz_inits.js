26fa258d80f37b6e0543a9674c5a1499cd8c759c
cferdinandi <hello@gomakethings.com>
2015-12-31 21:15:07 -0500

    Removed unneeded imgToggle.js script

--- src/js/petfinder-api/zzz_inits.js
+++ src/js/petfinder-api/zzz_inits.js
@@ -1,11 +1,1 @@
 petfinderSort.init();
-
-petfinderImgToggle.init({
-	selectorContainer: '.js-petfinder-img-container',
-	selectorImg: '.js-petfinder-img',
-	selectorToggle: '.js-petfinder-img-toggle',
-	imgAttributes: 'class="img-photo img-limit-height-large"',
-	callback: function () {
-		rightHeight.runRightHeight();
-	}
-});

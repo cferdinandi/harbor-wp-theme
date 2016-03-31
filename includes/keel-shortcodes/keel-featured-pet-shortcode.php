8b54394c20681ff9b3327454e7733e796b01f582
Mike iLL Kilmer <mike@mzoo.org>
2016-03-31 14:31:19 -0500

    Handle featured pet detached from orresponding post - replace with oldest.

--- includes/keel-shortcodes/keel-featured-pet-shortcode.php
+++ includes/keel-shortcodes/keel-featured-pet-shortcode.php
@@ -18,13 +18,27 @@
 			$this->type = $atts['type'];
 
 			$featured_pets = get_option( 'keel_featured_pet_selections', '');
-
-			if(isset($featured_pets['featured_pet_details']['name']) && $featured_pets['featured_pet_details']['name'] != ''):
-				return $this->generate_pet($featured_pets['featured_pet_details'], $featured_pets['featured_pet_post_id']);
-			elseif(isset($featured_pets['featured_pet_details']['name']) && $featured_pets['featured_pet_details']['name'] != ''):
-				return $this->generate_pet($featured_pets['featured_pet_backup_details'], $featured_pets['featured_pet_backup_post_id']);
+			
+			if(isset($featured_pets['featured_pet_details']['name']) && $featured_pets['featured_pet_details']['name'] != '' && True):
+				return $this->keel_generate_pet($featured_pets['featured_pet_details'], $featured_pets['featured_pet_post_id']);
+			elseif(isset($featured_pets['featured_pet_details']['name']) && $featured_pets['featured_pet_details']['name'] != '' && False):
+				return $this->keel_generate_pet($featured_pets['featured_pet_backup_details'], $featured_pets['featured_pet_backup_post_id']);
 			else:
-			// Retrieve the oldest pet
+				return $this->keel_retrieve_oldest_pet();
+			endif;
+		}
+		
+		public function keel_exceptions_error_handler($severity, $message, $filename, $lineno) {
+				if (error_reporting() == 0) {
+					return;
+				}
+				if (error_reporting() & $severity) {
+					throw new ErrorException($message, 0, $severity, $filename, $lineno);
+				}
+			}
+		
+		private function keel_retrieve_oldest_pet() {
+		// Retrieve single oldest pet from the DB
 			$args = array(
 				'showposts' => 1,
 				'post_type' => 'keel-pets',
@@ -37,24 +51,34 @@
 				$oldest_pet = new WP_Query($args);
 				if( $oldest_pet->have_posts() ) {
 					while ($oldest_pet->have_posts()) : $oldest_pet->the_post(); 
-						$featured_pet = $this->generate_pet('', $post->ID);
+						$featured_pet = $this->keel_generate_pet('', $post->ID);
 					endwhile; 
 				}
 				// Reset Post Data
 				wp_reset_postdata();
 				return $featured_pet;
-			endif;
 		}
 		
-		private function generate_pet($pet, $pet_post_id) {
+		private function keel_generate_pet($pet, $pet_post_id) {
+		
 			if( $pet == '' && $pet_post_id != ''):
 				$pet = get_post_meta( $pet_post_id, 'keel_pet_listings_pet_details', true );
 			endif;
+			
+			//Handle when featured pet is set, but not available among 'keel-pets'
+			set_error_handler(array($this, 'keel_exceptions_error_handler'));
+			
+			try {
+						// If we can't get a permalink, probably the post is moved or gone.
+						$post_permalink = get_post_permalink($pet_post_id);
+			} catch (Exception $e) {
+						return $this->keel_retrieve_oldest_pet();
+			}
+			
 			$return = '<article class="container">';
-
+			
 			$return .= '<h2 class="featured-pet-name">' . '<a class="keel-featured-pet" href="'.get_post_permalink($pet_post_id).'">' .
-									$pet['name'] . '</a></h2>';
-									
+						$pet['name'] . '</a></h2>';
 			if ($this->type == "full"):
 				$img = get_post_meta( $pet_post_id, 'keel_pet_listings_pet_imgs', true ); // All Images for this pet
 			else:
